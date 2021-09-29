9260ba40de7548d00a5c1c289f04be783f0f6568
zjw <newzjw713@163.com>
2021-09-29 17:49:38 +0800

    uni-app project

---
+++ components/index/index-list.vue
@@ -0,0 +1,187 @@
+<template>
+	<view class="index-list animated fadeIn slow">
+		<view class="index-list1">
+			<view class="avatar">
+				<image :src="item.userpic" mode="widthFix"></image>
+				{{ item.username }}
+			</view>
+			<view v-show="!isguanzhu" @tap="guanzhu"><view class="icon iconfont icon-zengjia follow">关注</view></view>
+		</view>
+		<view class="index-list2" @tap="opendetail">{{ item.title }}</view>
+		<view class="index-list3" @tap="opendetail">
+			<!-- 图片 -->
+			<image :src="item.titlepic" mode="widthFix"></image>
+			<!-- 视频 -->
+			<template v-if="item.type == 'video'">
+				<view class="icon iconfont icon-bofang index-list-play"></view>
+				<view class="index-list-playinfo">{{ item.playnum }}次播放 {{ item.long }}</view>
+			</template>
+		</view>
+		<view class="index-list4">
+			<view class="left">
+				<view :class="{ active: infonum.index == 1 }" @tap="caozuo('ding')">
+					<view class="icon iconfont icon-icon_xiaolian-mian"></view>
+					{{ infonum.dingnum }}
+				</view>
+				<view :class="{ active: infonum.index == 2 }" @tap="caozuo('cai')">
+					<view class="icon iconfont icon-kulian"></view>
+					{{ infonum.cainum }}
+				</view>
+			</view>
+			<view class="right">
+				<view>
+					<view class="icon iconfont icon-pinglun1"></view>
+					{{ item.commentnum }}
+				</view>
+				<view>
+					<view class="icon iconfont icon-zhuanfa"></view>
+					{{ item.sharenum }}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	props: {
+		item: Object,
+		index: Number
+	},
+	data() {
+		return {
+			isguanzhu: this.item.isguanzhu,
+			infonum: this.item.infonum
+		};
+	},
+	methods: {
+		// 关注
+		guanzhu() {
+			this.isguanzhu = true;
+			uni.showToast({
+				title: '关注成功'
+			});
+		},
+		// 顶踩
+		caozuo(type) {
+			switch (type) {
+				case 'ding':
+					if (this.infonum.index == 1) {
+						return;
+					}
+					this.infonum.dingnum++;
+					if (this.infonum.index == 2) {
+						this.infonum.cainum--;
+					}
+					this.infonum.index = 1;
+					break;
+				case 'cai':
+					if (this.infonum.index == 2) {
+						return;
+					}
+					this.infonum.cainum++;
+					if (this.infonum.index == 1) {
+						this.infonum.dingnum--;
+					}
+					this.infonum.index = 2;
+					break;
+			}
+		},
+		// 进入详情页
+		opendetail() {
+			uni.navigateTo({
+				url: '../../pages/detail/detail?detailData='+JSON.stringify(this.item)
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.index-list {
+	padding: 10px;
+	border-bottom: 1px solid #eeeeee;
+	.index-list1 {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		.avatar {
+			display: flex;
+			align-items: center;
+			image {
+				width: 84rpx;
+				height: 84rpx;
+				border-radius: 50%;
+				margin-right: 10rpx;
+			}
+		}
+		.follow {
+			background: #f4f4f4;
+			border-radius: 5upx;
+			padding: 0 10upx;
+		}
+	}
+	.index-list2 {
+		padding-top: 14rpx;
+		font-size: 32rpx;
+	}
+	.index-list3 {
+		padding-top: 14rpx;
+		position: relative;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		image {
+			width: 100%;
+			border-radius: 20rpx;
+		}
+		.index-list-play {
+			position: absolute;
+			font-size: 140rpx;
+			color: #ffffff;
+		}
+		.index-list-playinfo {
+			position: absolute;
+			background: rgba(51, 51, 51, 0.72);
+			color: #ffffff;
+			bottom: 4px;
+			right: 4px;
+			border-radius: 20px;
+			font-size: 11px;
+			padding: 0 6px;
+		}
+	}
+	.index-list4 {
+		padding: 14rpx 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		.left {
+			display: flex;
+			view {
+				margin-right: 10rpx;
+				display: flex;
+				view {
+					margin-right: 10rpx;
+				}
+			}
+			.active {
+				color: #c5f61c;
+				view {
+					color: #c5f61c;
+				}
+			}
+		}
+		.right {
+			display: flex;
+			view {
+				display: flex;
+				margin-right: 10rpx;
+				view {
+					margin-right: 10rpx;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ pages/user-chat/user-chat.vue
@@ -0,0 +1,117 @@
+<template>
+	<view>
+		
+		<scroll-view id="scrollview" scroll-y :scroll-top="scrollTop" 
+		:scroll-with-animation="true"
+		:style="{height:style.contentH+'px'}">
+			<!-- 聊天列表 -->
+			<block v-for="(item,index) in list" :key="index">
+				<user-chat-list :item="item" :index="index"></user-chat-list>
+			</block>
+		</scroll-view>
+		
+		<!-- 输入框 -->
+		<user-chat-bottom @submit="submit"></user-chat-bottom>
+	</view>
+</template>
+
+<script>
+	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue";
+	import time from "../../common/time.js";
+	import userChatList from "../../components/user-chat/user-chat-list.vue";
+	export default {
+		components:{
+			userChatBottom,
+			userChatList
+		},
+		data() {
+			return {
+				scrollTop:0,
+				style:{
+					contentH:0,
+					itemH:0
+				},
+				list:[]
+			};
+		},
+		onLoad() {
+			this.getdata();
+			this.initdata();
+		},
+		onReady() {
+			this.pageToBottom();
+		},
+		methods:{
+			// 初始化参数
+			initdata(){
+				try {
+					const res = uni.getSystemInfoSync();
+					// res.windowHeight当前窗口高度 - 底部输入框高度
+					this.style.contentH=res.windowHeight - uni.upx2px(120);
+				} catch (e) { }
+			},
+			// 滚动到底部
+			// scrollTop滚动条位置 = 所有聊天气泡高度之和
+			pageToBottom(){
+				let q=uni.createSelectorQuery();
+				q.select('#scrollview').boundingClientRect();
+				q.selectAll('.user-chat-item').boundingClientRect();
+				
+				q.exec((res)=>{
+					res[1].forEach((ret)=>{
+						this.style.itemH += ret.height;
+					});
+					// 如果总高度超过可视区域高度，就让滚动条的位置发生变化
+					if(this.style.itemH > this.style.contentH){
+						this.scrollTop=this.style.itemH;
+					}
+					
+				})
+			},
+			// 获取聊天数据
+			getdata(){
+				// 从服务器获取到的数据
+				let arr=[
+					{
+						isme:false,
+						userpic:"../../static/demo/userpic/11.jpg",
+						type:"text",
+						data:"哈哈哈",
+						time:"1555146412"
+					},
+					{
+						isme:true,
+						userpic:"../../static/demo/userpic/10.jpg",
+						type:"img",
+						data:"../../static/demo/3.jpg",
+						time:"1555146414",
+					},
+				];
+				for (let i = 0; i < arr.length; i++) {
+					// 给每个数据加一个转换后的时间，getChatTime相差300秒才显示上一条时间
+					arr[i].gstime=time.gettime.getChatTime(arr[i].time,i>0?arr[i-1].time:0);
+				}
+				this.list=arr;
+			},
+			submit(data){
+				// 构建数据
+				let now=new Date().getTime();
+				let obj={
+					isme:true,
+					userpic:"../../static/demo/userpic/10.jpg",
+					type:"text",
+					data:data,
+					time:now,
+					gstime:time.gettime.getChatTime(now,this.list[this.list.length-1].time)
+				};
+				this.list.push(obj);
+				// 每次发送聊天内容的时候都要执行滚动到底部
+				this.pageToBottom();
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
